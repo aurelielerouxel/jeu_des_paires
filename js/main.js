e639b59d4870119335e01be665195b9dfa71dcd0
Aurelie Lerouxel <malkiniou@gmail.com>
2020-09-05 12:56:28 +0200

    time limite option

--- js/main.js
+++ js/main.js
@@ -16,7 +16,6 @@ let imgFront = [
 ];
 
 let back = document.getElementsByClassName("back");
-// let coups = document.getElementById("score");
 
     let choices = [];
     let pair = [];
@@ -30,9 +29,7 @@ let back = document.getElementsByClassName("back");
             let imgRandom = imgFront[Math.floor(Math.random() * [i])];
             this.src = imgRandom;
             choices.push(this);
-
-            console.log(choices);
-            
+            console.log(choices);          
 
             // si deux éléments se trouvent dans le tableau 
             if (choices.length == 2) {  
@@ -43,11 +40,13 @@ let back = document.getElementsByClassName("back");
                     console.log("BG");
                     pair.push(this);
                     console.log(pair);
+                    // et je vide le tableau
                     choices.pop(this);
                     choices.pop(this); 
                     score++;
 
                     // impossibilité au double-clic sur une même carte
+                    // si la carte est côté front, ne pas pouvoir recliquer sur la carte
 
                     // si toutes les cartes sont retournée (2ème array = 6) alors fin de partie
                     if (pair.length === 6) {
@@ -57,19 +56,37 @@ let back = document.getElementsByClassName("back");
                 // s'ils sont différents
                 else {
                     console.log("Tnul");
+                    // mise en place d'un timer pour avoir le temps de voir les 2 cartes retournées
                     setTimeout(function() {
                         choices[0].src = "img/card.png",
                         choices[1].src = "img/card.png";
+                        // et je vide le tableau
                         choices.pop(this);
                         choices.pop(this);
                         score++; 
                     }, 500);          
                 }
 
+                // Affiche le nombre de coups sur une partie
                 console.log(score);
                 document.getElementById("score").innerHTML = "Score : " + score;
                 
             }
-        })
+        })      
     }
+            // Difficuté avec un temps limité
+            let timer = document.getElementById("timer");
+            let sec = 5;
+            
+            var time = setInterval(function() {
+                sec--;
+                // console.log(sec);
+                timer.innerText = sec; 
+                // console.log(timer.innerText); 
+                if (sec === 0) {
+                    alert("Game over");
+                    clearInterval(time);
+                }    
+            }, 1000);
+           
     // recommencer une partie
