e10cba7f653b9f209f5667d436ad176b772c81c0
Aurelie Lerouxel <malkiniou@gmail.com>
2020-09-06 18:52:12 +0200

    options and optimization still to be done

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-// Au clic supprimer le hidden du front et le mettre sur le back
-let imgFront = [
+
+const imgFront = [
     'img/css.png', 
-    'img/github.png',
+    'img/microsoft.png',
     'img/html5.png',
+    'img/github.png',
+    'img/json.png',
+    'img/microsoft.png',     
+    'img/html5.png',
+    'img/github.png',
+    'img/javascript.png',
     'img/javascript.png',
     'img/json.png',
-    'img/microsoft.png',
     'img/css.png', 
-    'img/github.png',
-    'img/html5.png',
-    'img/javascript.png',
-    'img/json.png',
-    'img/microsoft.png',
 ];
 
 let back = document.getElementsByClassName("back");
@@ -20,20 +20,41 @@ let choices = [];
 let pair = [];
 let drawCard = [];
 let score = 0;
-let coups = 1;
+let coups = 10;
 
 // function jouer() { 
 //     alert("Bonjour");
 //     console.log(jouer);
 //     console.log(alert);
-// 
+
+    // function randomImg() {
+    //     const imgFront = [
+    //         'img/css.png', 
+    //         'img/microsoft.png',
+    //         'img/html5.png',
+    //         'img/github.png',
+    //         'img/json.png',
+    //         'img/microsoft.png',     
+    //         'img/html5.png',
+    //         'img/github.png',
+    //         'img/javascript.png',
+    //         'img/javascript.png',
+    //         'img/json.png',
+    //         'img/css.png',        
+    //     ];
+    //     let imgRandom = Math.floor(Math.random() * Math.floor(imgFront.length));
+    //     return imgFront[imgRandom];
+    // }
+
     // positionnement aléatoire des cartes du tableau
+    // for()
     for(let i=0; i<back.length; i++) {
-        back[i].addEventListener('click', function() {
-            let imgRandom = imgFront[Math.floor(Math.random() * [i])];
+        back[i].addEventListener('click', function(randomImg) {
+            let imgRandom = imgFront[i];
+            drawCard.push(imgRandom);
             this.src = imgRandom;
             choices.push(this);
-            console.log(choices);          
+            console.log(choices);     
 
             // si deux éléments se trouvent dans le tableau 
             if (choices.length == 2) {  
@@ -44,14 +65,19 @@ let coups = 1;
                     console.log("BG");
                     pair.push(this);
                     console.log(pair);
-                    // et je vide le tableau
+                    // et on vide le tableau
                     choices.pop(this);
                     choices.pop(this); 
+                    // on incrémente et décrémente
                     score++;
                     coups--;
 
                     // impossibilité au double-clic sur une même carte
                     // (si la carte est côté front, ne pas pouvoir recliquer sur la carte)
+                    if(choices != back) {
+                        console.log("interdit de cliquer 2 fois sur la même carte");
+                        
+                    }
 
                     // si toutes les cartes sont retournée (2ème array = 6) alors fin de partie
                     if (pair.length === 6) {
@@ -64,40 +90,41 @@ let coups = 1;
                     console.log("Tnul");
                     // mise en place d'un timer pour avoir le temps de voir les 2 cartes retournées
                     setTimeout(function() {
-                        choices[0].src = "img/card.png",
+                        choices[0].src = "img/card.png";
                         choices[1].src = "img/card.png";
-                        // et je vide le tableau
+                        // et on vide le tableau
                         choices.pop(this);
                         choices.pop(this);
+                        // on incrémente et décrémente
                         score++; 
                         coups--;
                     }, 500);          
                 }
-
+                
                 // Affiche le nombre de coups sur une partie
                 console.log(score);
                 document.getElementById("score").innerHTML = "Score : " + score;
 
                 // Difficulté avec un nombre de coups maximum
                 document.getElementById("coups").innerText = "Coups : " + coups;
-                if (coups === 0) {
-                    alert("Game over");
-                    console.log("looser");
-                }             
+                // if (coups === 0) {
+                //     alert("Game over");
+                //     console.log("looser");
+                // }             
             }
         })      
     }
 
     // Difficuté avec un temps limité
     let timer = document.getElementById("timer");
-    let sec = 5;
+    let sec = 60;
             
     let time = setInterval(function() {
         sec--;
         // console.log(sec);
         timer.innerText = sec; 
         // console.log(timer.innerText); 
-        if (sec === 0) {
+        if (sec === 0 || coups === 0) {
             alert("Game over");
             clearInterval(time);
         }    
@@ -113,3 +140,6 @@ let coups = 1;
 //         continuer = confirm("Veux tu continuer ?");             
 //     }
 // }
+
+
+// faire un jeu de morpion, puissance 4
